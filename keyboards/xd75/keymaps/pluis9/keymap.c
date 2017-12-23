6c62dac0e46655cc7f69b1d4ca3bd8597c15d27d
MakotoKurauchi <pluis@me.com>
2017-12-23 11:29:02 +0900

    add eucalyn

--- keyboards/xd75/keymaps/pluis9/keymap.c
+++ keyboards/xd75/keymaps/pluis9/keymap.c
@@ -19,14 +19,16 @@
 #define _QWERTY 0
 #define _COLEMAK 1
 #define _DVORAK 2
-#define _LOWER 3
-#define _RAISE 4
+#define _EUCALYN 3
+#define _LOWER 4
+#define _RAISE 5
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
   DVORAK,
+  EUCALYN,
   LOWER,
   RAISE,
   EISU,
@@ -51,11 +53,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = {
   { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_KP_7, KC_KP_8, KC_KP_9, KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_KP_4, KC_KP_5, KC_KP_6, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_PIPE },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_KP_4, KC_KP_5, KC_KP_6, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS },
   { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_KP_1, KC_KP_2, KC_KP_3, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_KP_0, KC_RBRC, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
   { MO(_ADJUST), KC_GRV, KC_LALT, KC_LGUI, EISU, LOWER,   KC_PENT, KC_PDOT, KC_SPC,  RAISE,  KANA,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
  },
+
+ /* EUCALYN
+  * .--------------------------------------------------------------------------------------------------------------------------------------.
+  * | ESC    | 1      | 2      | 3      | 4      | 5      | 7      | 8      | 9      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+  * | TAB    | /      | ,      | .      | F      | Q      | 4      | 5      | 6      | M      | R      | D      | Y      | P      | \      |
+  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+  * | LCTRL  | A      | O      | E      | I      | U      | 1      | 2      | 3      | G      | T      | K      | S      | N      | ENTER  |
+  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+  * | LSHIFT | Z      | X      | C      | V      | W      | [      | 0      | ]      | B      | H      | J      | L      | ;      | RSHIFT |
+  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
+  * | ADJUST | `      | LALT   | GUI    | EISU   | LOWER  | ENTER  | .      | SPACE  | RAISE  | KANA   | LEFT   | DOWN   | UP     | RIGHT  |
+  * '--------------------------------------------------------------------------------------------------------------------------------------'
+  */
+
+ [_EUCALYN] = {
+   { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_KP_7, KC_KP_8, KC_KP_9, KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
+   { KC_TAB,  KC_BSLS, KC_BSLS, KC_DOT,  KC_F,    KC_Q,    KC_KP_4, KC_KP_5, KC_KP_6, KC_M,   KC_R,   KC_D,    KC_Y,    KC_P,    KC_BSLS },
+   { KC_LCTL, KC_A,    KC_O,    KC_E,    KC_I,    KC_U,    KC_KP_1, KC_KP_2, KC_KP_3, KC_G,   KC_T,   KC_K,    KC_S,    KC_N,    KC_ENT },
+   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_W,    KC_LBRC, KC_KP_0, KC_RBRC, KC_B,   KC_H,   KC_J,    KC_L,    KC_SCLN, KC_RSFT },
+   { MO(_ADJUST), KC_GRV, KC_LALT, KC_LGUI, EISU, LOWER,   KC_PENT, KC_PDOT, KC_SPC,  RAISE,  KANA,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
+  },
 
 /* LOWER
  * .--------------------------------------------------------------------------------------------------------------------------------------.
@@ -105,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * |        |        |        |        |        |        |        |        |        |        |        |        |        |        | DELETE |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | RESET  |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |        | RESET  |        |        |        |        |        |        |        | QWERTY |EUCALYN |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        |        |        |        | Mac    |        |        |        | Win    |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -117,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_ADJUST] = {
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL },
-  { _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, RESET,   _______, _______, _______, _______, _______, _______, _______, QWERTY,  EUCALYN, _______, _______, _______, _______ },
   { _______, _______, _______, _______, _______, AG_NORM, _______, _______, _______, AG_SWAP, _______, _______, _______, _______, _______ },
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI },
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SMOD, RGB_HUD, RGB_SAD, RGB_VAD },
@@ -167,6 +191,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
+        case EUCALYN:
+            if (record->event.pressed) {
+              set_single_persistent_default_layer(_EUCALYN);
+            }
+            return false;
+            break;
         case LOWER:
           if (record->event.pressed) {
             layer_on(_LOWER);
